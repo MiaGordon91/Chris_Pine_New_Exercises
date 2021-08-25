de1415e872db08f64a8b52064b9c26eed1928b44
My21bernate <mia.gordon@digital.justice.gov.uk>
2021-08-25 10:18:04 +0100

    Mixing it up exercises

---
+++ mixing-it-up.rb
@@ -0,0 +1,21 @@
+#Mixing it Up
+#full name greeting
+
+puts "Please enter your first name:"
+first_name = gets.chomp
+
+puts "Please enter your middle name:"
+middle_name = gets.chomp
+
+puts "Please enter your surname:"
+surname = gets.chomp
+
+puts "Welcome #{first_name} #{middle_name} #{surname}!"
+
+
+#Bigger, better fav number
+ puts "Enter your favourite number:"
+ num = gets.chomp.to_i
+ better_num = num + 1
+
+ puts "I think you'll like this number better: #{better_num}!"
