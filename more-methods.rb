c257bfd1e91deb81880b9a2d0eed85003781731f
My21bernate <mia.gordon@digital.justice.gov.uk>
2021-08-25 12:24:25 +0100

    More about methods exercises

---
+++ more-methods.rb
@@ -0,0 +1,25 @@
+
+#methods
+#Angry boss program
+
+puts "What do you want?"
+
+response = gets.chomp.upcase
+
+puts "WHADDYA MEAN \"#{response}\" ?!? YOU\'RE FIRED!!"
+
+
+
+#Table of contents
+
+title = "Table of contents".center(70)
+line_break = ""
+line_one = "Chapter 1:  Getting Started".ljust(30) + " page 2".rjust(20)
+line_two = "Chapter 2:  Numbers".ljust(30) + "page 9".rjust(20)
+line_three = "Chapter 3:  Letters".ljust(30) + " page 13".rjust(21)
+
+puts title
+puts line_break
+puts line_one
+puts line_two
+puts line_three
