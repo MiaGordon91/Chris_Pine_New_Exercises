f66173b81b9b0dadb33656209d9cc37bdd113b32
My21bernate <mia.gordon@digital.justice.gov.uk>
2021-08-25 10:05:59 +0100

    number exercises

---
+++ numbers.rb
@@ -0,0 +1,19 @@
+#Numbers
+#Hours in a year
+day_hours = 24
+week_hours = 24 * 7
+
+puts week_hours * 52
+
+#minutes in a decade
+mins_day = 60 * 24
+mins_week = mins_day * 7
+mins_year = mins_week * 52
+
+puts mins_year * 10
+
+#authors age
+author_days = 1025000000 / 86400
+author_years = author_days / 365
+puts author_years
+#32
