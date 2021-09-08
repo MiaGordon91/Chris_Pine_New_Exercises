d267f5f15080ef41e3131feb919ca2848bc0ccf9
My21bernate <mia.gordon@digital.justice.gov.uk>
2021-09-08 14:45:48 +0100

    additional exercise completed

--- classes_of_objects.rb
+++ classes_of_objects.rb
@@ -1,4 +1,4 @@
-=begin
+
 #one billion seconds
 
 birthdate = Time.local(1991, 4,7)
@@ -14,7 +14,6 @@ minus_billion = seconds_alive - 1000000000
 #puts minus_billion
 puts todays_date - minus_billion
 
-=end
 
 
 #Happy Birthday
@@ -55,4 +54,29 @@ end
   result = result_numbers/3.154e+7
 
   puts
-  puts "You are #{result.to_i} years old!"
+  puts "You are #{result.to_i} years old!\nHere's a SPANK! for each birthday you've had..."
+  puts
+  (result.to_i).times {puts "SPANK!"}
+
+
+#Party like it's...
+
+def integer_roman(letters)
+
+  rom_num = {"M"=> 1000, "D" => 500, "C" => 100, "L"=> 50, "X" => 10, "V" => 5,"I" => 1}
+
+  answer = 0
+
+  letters.each do |letter|
+    if !rom_num.include?(letter)
+      return "Please enter a valid string of roman numerals - M,D,C,L,X,V,I"
+    end
+
+    answer += rom_num[letter]
+  end
+   return answer
+ end
+
+puts "Enter random letters"
+i = gets.chomp().upcase.split(//)
+puts integer_roman(i)
